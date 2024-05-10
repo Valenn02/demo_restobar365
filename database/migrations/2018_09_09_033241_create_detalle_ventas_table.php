03306e3657e0502df0a55ad9a1273abf0be9d600
JuanCarlosSandy <juan.sandy@uab.edu.bo>
2024-05-10 18:17:30 -0400

    arreglo de codigo envez de idaproducto

--- database/migrations/2018_09_09_033241_create_detalle_ventas_table.php
+++ database/migrations/2018_09_09_033241_create_detalle_ventas_table.php
@@ -17,8 +17,7 @@ class CreateDetalleVentasTable extends Migration
             $table->increments('id');
             $table->integer('idventa')->unsigned();
             $table->foreign('idventa')->references('id')->on('ventas')->onDelete('cascade');
-            $table->integer('idarticulo')->unsigned();
-            $table->foreign('idarticulo')->references('id')->on('articulos');
+            $table->integer('codigoComida');
             $table->integer('cantidad');
             $table->decimal('precio', 11, 2);
             $table->decimal('descuento', 11, 2);
